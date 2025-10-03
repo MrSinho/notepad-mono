0226234deedb0476a46c6a3d15579a9d47c07b1b
MrSinho <sino3791@gmail.com>
2025-10-03 14:16:58 +0200

    .

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -6,83 +6,48 @@ import 'backend/supabase/auth_access.dart';
 import 'backend/app_data.dart';
 import 'backend/navigator.dart';
 
-import 'static/ui_utils.dart';
-
 import 'themes.dart';
-
-
-
-WidgetBuilder futureBuilder(
-  Future<dynamic> async_widget,
-) {
-
-  return (BuildContext context) { return futureBuilderWidget(async_widget); };
-
-}
-
-Future<Widget> goToHomePage() async {
-  await storeUserData();
-  return Text("Hello");
-}
-
 
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     MaterialApp app = MaterialApp(
-      debugShowCheckedModeBanner: false,
-      navigatorKey: NavigatorInfo.key,
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NavigatorInfo.key,
+        home: StreamBuilder<AuthState>(
+            stream: Supabase.instance.client.auth.onAuthStateChange,
+            builder: (context, snapshot) {
+              AppData.instance.setupWithContext(context);
 
-      home: StreamBuilder<AuthState>(
+              if (snapshot.data != null && snapshot.data!.session != null) {
+                FutureBuilder builder = FutureBuilder<void>(
+                    future: storeUserData(),
+                    builder: (context, userSnapshot) {
+                      if (userSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        // Loading screen
 
-        stream: Supabase.instance.client.auth.onAuthStateChange,
+                        Scaffold loadingScaffold = const Scaffold(
+                            body: SizedBox(width: 10.0, height: 10.0));
 
-        builder: (context, snapshot) {
+                        return loadingScaffold;
+                      }
 
-          AppData.instance.setupWithContext(context);
-          
-          if (snapshot.data != null && snapshot.data!.session != null) {
+                      return AppData.instance.homePage;
+                    });
 
-            FutureBuilder builder = FutureBuilder<void>(
-              future: storeUserData(),
-              builder: (context, userSnapshot) {
-
-                if (userSnapshot.connectionState == ConnectionState.waiting) {// Loading screen
-                  
-                  Scaffold loadingScaffold = const Scaffold(
-                    body: SizedBox(width: 10.0, height: 10.0)
-                  );
-                  
-                  return loadingScaffold;
-                }
-
-                return AppData.instance.homePage;
+                return builder;
+              } else {
+                return AppData.instance.loginPage;
               }
-            );
+            }),
+        theme: brightTheme(),
+        darkTheme: darkTheme(),
+        highContrastTheme: brightThemeHighContrast(),
+        highContrastDarkTheme: darkThemeHighContrast());
 
-            return builder;
-          }
-
-          else {
-            return AppData.instance.loginPage;
-          }
-
-        }
-      ),
-
-      theme: brightTheme(),
-
-      darkTheme: darkTheme(),
-
-      highContrastTheme: brightThemeHighContrast(),
-
-      highContrastDarkTheme: darkThemeHighContrast()
-    );
-
-    return app; 
+    return app;
   }
-
 }
