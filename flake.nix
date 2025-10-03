ebd8eebc681cd24f2b2e1a25263d599c89acee7a
MrSinho <sino3791@gmail.com>
2025-10-03 17:13:24 +0200

    .

--- flake.nix
+++ flake.nix
@@ -120,17 +120,19 @@
                             
             done
 
-            #patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/libapp.so
-            #patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/libflutter_linux_gtk.so
-            #patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/libgtk_plugin.so
-            #patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/liburl_launcher_linux_plugin.so
+            patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/libapp.so
+            patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/libflutter_linux_gtk.so
+            patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/libgtk_plugin.so
+            patchelf --remove-rpath $PWD/build/linux/x64/release/bundle/lib/liburl_launcher_linux_plugin.so
 
             cp $PWD/build/linux/x64/release/bundle/notepad_mono  $out/linux/notepad_mono
-            #cp -r $PWD/build/linux/x64/release/bundle/lib/*         $out/linux/lib
+            cp -r $PWD/build/linux/x64/release/bundle/lib/*         $out/linux/lib
           '';
           
-          postFixup = ''
+          postFixup = ''# Patch executable to find shared libraries
+            # patchelf -d $out/linux/notepad_mono
             patchelf --add-rpath ${pkgs.libepoxy}/lib $out/linux/notepad_mono
+            patchelf --add-rpath ${pkgs.libgtk}/lib $out/linux/notepad_mono
           '';
 
         }
