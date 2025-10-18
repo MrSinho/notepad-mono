1257cc1daa3751615a2e2021e05393eb643ffb0d
MrSinho <sino3791@gmail.com>
2025-10-18 20:47:30 +0200

    Flake fix attempts

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs = {
+  inputs = {# Flake imports
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
@@ -7,9 +7,28 @@
       url = "github:numtide/flake-utils";
     };
   };
+
   outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs {
+
+      linuxPipeline = import ./nix/linuxPipeline.nix { inherit system nixpkgs; };
+
+      buildTarget = 
+        let
+          _buildTarget = builtins.getEnv "build_target";
+        in
+          if _buildTarget == "" || _buildTarget == null then "linux"
+          else _buildTarget;
+
+      #
+      # Package imports
+      #
+
+      windowsPkgs = import nixpkgs {
+        inherit system;
+      };
+
+      androidPkgs = import nixpkgs {
         inherit system;
         config = {
           allowUnfree = true;
@@ -18,7 +37,7 @@
       };
 
       # https://discourse.nixos.org/t/android-ndk-and-sdk-licenses-not-accepted/68215/2
-      androidComposition = pkgs.androidenv.composeAndroidPackages {
+      androidComposition = androidPkgs.androidenv.composeAndroidPackages {
         buildToolsVersions = [ "34.0.0" "35.0.0" ];
         platformVersions = [ "34" "35" "36" ];
         
@@ -38,133 +57,123 @@
 
       androidSdk = androidComposition.androidsdk;
 
-      notepad-mono = (with pkgs; stdenv.mkDerivation {
-          pname = "Notepad Mono";
-          version = "1.0.0";
-          
-          src = ./.;
+      #
+      # Build inputs
+      #
 
-          #src = pkgs.fetchgit {
-          #  url = "https://github.com/mrsinho/notepad-mono.git";
-          #  rev = "9b053e06b2aa55ba25d467d284f0ef62dbffdf9c";
-          #  sha256 = "0000000000000000000000000000000000000000000000000000";# dummy 0000000000000000000000000000000000000000000000000000
-          #  fetchSubmodules = true;
-          #};
+      windowsBuildInputs = [
+        windowsPkgs.flutter
+        windowsPkgs.wineWowPackages.full
+        windowsPkgs.wineWowPackages.waylandFull
+        windowsPkgs.winetricks
+      ];
 
-          nativeBuildInputs = [
-            pkgs.flutter
+      androidBuildInputs = [
+        androidPkgs.flutter
+        androidSdk
+        androidPkgs.gradle
+        androidPkgs.jdk17
+      ];
+      
+      #
+      # Build phases
+      #
 
-            pkgs.patchelf
+      androidBuildPhase = ''
+        export XDG_CACHE_HOME=$TMPDIR/.cache
+        export XDG_CONFIG_HOME=$TMPDIR/config
+        export GRADLE_USER_HOME=$TMPDIR/.gradle
+        export ANDROID_USER_HOME=$TMPDIR/.android
+        
+        export JAVA_HOME="${androidPkgs.jdk17}/lib/openjdk"
+        export PATH="$JAVA_HOME/bin:$PATH"
+        
+        export ANDROID_HOME="${androidSdk}/libexec/android-sdk"
+        export PATH="${androidSdk}/platform-tools:$PATH"
+        export PATH="${androidSdk}/cmdline-tools/latest/bin:$PATH"
+        
+        # Add build-tools so we can use the Nix-packaged aapt2 instead of Maven’s
+        export PATH="${androidSdk}/build-tools/35.0.0:$PATH" # !
+        export PATH="${androidSdk}/build-tools/34.0.0:$PATH" # !
 
-            pkgs.pango
-            pkgs.cairo
-            pkgs.glib
-            pkgs.libepoxy
-            pkgs.fontconfig
-            pkgs.gdk-pixbuf
-            pkgs.harfbuzz
-            pkgs.xorg.libX11
-            pkgs.libdeflate
+        # Force AGP to use aapt2 from SDK/build-tools rather than Maven cache (fixes NixOS stub-ld issue)
+        export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_HOME/build-tools/35.0.0/aapt2 $GRADLE_OPTS"
 
-            androidSdk
-            pkgs.gradle
-            pkgs.jdk
-          ];
+        # Ensure directories exist and are writable
+        mkdir -p $XDG_CACHE_HOME $XDG_CONFIG_HOME $GRADLE_USER_HOME $ANDROID_USER_HOME 
 
-          buildPhase = ''
-            export FLUTTER_STORAGE_BASE_DIR=$TMPDIR/flutter_storage
+        cd app
+        flutter pub get
+        flutter create .
 
-            export HOME=$TMPDIR
-            export XDG_CACHE_HOME=$TMPDIR/.cache
-            export XDG_CONFIG_HOME=$TMPDIR/config
-            export GRADLE_USER_HOME=$TMPDIR/.gradle
-            export ANDROID_USER_HOME=$TMPDIR/.android
-            
-            export JAVA_HOME="${jdk17}/lib/openjdk"
-            export PATH="$JAVA_HOME/bin:$PATH"
-            
-            export ANDROID_HOME="${androidSdk}/libexec/android-sdk"
-            export PATH="${androidSdk}/platform-tools:$PATH"
-            export PATH="${androidSdk}/cmdline-tools/latest/bin:$PATH"
-            
-            # Add build-tools so we can use the Nix-packaged aapt2 instead of Maven’s
-            export PATH="${androidSdk}/build-tools/35.0.0:$PATH" # !
-            export PATH="${androidSdk}/build-tools/34.0.0:$PATH" # !
+        flutter build apk --release
+      '';
 
-            # Force AGP to use aapt2 from SDK/build-tools rather than Maven cache (fixes NixOS stub-ld issue)
-            export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=$ANDROID_HOME/build-tools/35.0.0/aapt2 $GRADLE_OPTS"
+      #
+      # Install phases
+      #
 
-            # Ensure directories exist and are writable
-            mkdir -p $HOME $FLUTTER_STORAGE_BASE_DIR  $XDG_CACHE_HOME $XDG_CONFIG_HOME $GRADLE_USER_HOME $ANDROID_USER_HOME 
+      androidInstallPhase = ''# $PWD starts from app directory
+        mkdir -p $out/android
+        cp -r $PWD/build/app/outputs/flutter-apk/app-release.apk $out/android/notepad_mono.apk
+        #cp -r $PWD/build/app/outputs/flutter-apk/app-debug.apk $out/android/notepad_mono-debug.apk
+      '';
 
-            #flutter doctor
+      #
+      # Evaluate platform
+      #
 
-            cd app
-            flutter pub get
-            flutter create .
-            
-            flutter build apk --release
-            flutter build linux --release
-          '';
+      platformBuildInputs = 
+        if buildTarget == "linux" then linuxPipeline.buildInputs
+        else if buildTarget == "android" then androidBuildInputs
+        else throw "Unsupported target platform: ${buildTarget}";
 
-          installPhase = ''# $PWD starts from app directory
+      platformBuildPhase = 
+        if buildTarget == "linux" then linuxPipeline.buildPhase
+        else if buildTarget == "android" then androidBuildPhase
+        else throw "Unsupported target platform: ${buildTarget}";
 
-            mkdir -p $out/linux
-            mkdir -p $out/android
+      platformInstallPhase = 
+        if buildTarget == "linux" then linuxPipeline.installPhase
+        else if buildTarget == "android" then androidInstallPhase
+        else throw "Unsupported target platform: ${buildTarget}";
 
-            #
-            # ANDROID
-            #
+      #
+      # Pipeline
+      #
 
-            cp -r $PWD/build/app/outputs/flutter-apk/app-release.apk $out/android/notepad_mono.apk
-            #cp -r $PWD/build/app/outputs/flutter-apk/app-debug.apk $out/android/notepad_mono-debug.apk
+      notepad-mono = (linuxPipeline.pkgs.stdenv.mkDerivation {
 
-            #
-            # LINUX
-            #
+        pname = "Notepad Mono";
+        version = "1.0.0";
+        
+        src = ./.;
 
-            # Replace broken shared library paths with safe packages from nix store
-            for so in $PWD/build/linux/x64/release/bundle/lib/*.so; do
-              echo "Required libraries for $(basename "$so")" >> $out/linux/readelf.txt
-              
-              if readelf -d "$so" | grep -q RUNPATH; then
-                readelf -d "$so" | grep RUNPATH | tr ":" "\n" | tr "[" "\n" | tr "]" "\n" >> $out/linux/readelf.txt
+        #src = pkgs.fetchgit {
+        #  url = "https://github.com/mrsinho/notepad-mono.git";
+        #  rev = "9b053e06b2aa55ba25d467d284f0ef62dbffdf9c";
+        #  sha256 = "0000000000000000000000000000000000000000000000000000";# dummy 0000000000000000000000000000000000000000000000000000
+        #  fetchSubmodules = true;
+        #};
 
-                patchelf --set-rpath ${pkgs.pango}/lib $so
-                patchelf --add-rpath ${pkgs.cairo}/lib $so
-                patchelf --add-rpath ${pkgs.glib}/lib $so
-                patchelf --add-rpath ${pkgs.libepoxy}/lib $so
-                patchelf --add-rpath ${pkgs.fontconfig.lib}/lib $so
-                patchelf --add-rpath ${pkgs.gdk-pixbuf}/lib $so
-                patchelf --add-rpath ${pkgs.harfbuzz}/lib $so
-                patchelf --add-rpath ${pkgs.xorg.libX11}/lib $so
-                patchelf --add-rpath ${pkgs.libdeflate}/lib $so
-                
-              else
-                  echo "(no RUNPATH)" >> $out/linux/readelf.txt
-                  echo " " >> $out/linux/readelf.txt
-              fi
-                            
-            done
+        nativeBuildInputs = platformBuildInputs;
 
-            # Copy bundle folder to output
-            cp -r $PWD/build/linux/x64/release/bundle/* $out/linux
+        buildPhase = ''
+          export FLUTTER_STORAGE_BASE_DIR=$TMPDIR/flutter_storage
+          export HOME=$TMPDIR
+          mkdir -p $HOME $FLUTTER_STORAGE_BASE_DIR
 
-            # Patch also executable to find shared libraries
-            # readelf -d $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.cairo}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.glib}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.libepoxy}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.fontconfig.lib}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.gdk-pixbuf}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.harfbuzz}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.xorg.libX11}/lib $out/linux/notepad_mono
-            patchelf --add-rpath ${pkgs.libdeflate}/lib $out/linux/notepad_mono
-          '';
+          ${platformBuildPhase}
+        '';
+        
+        installPhase = ''
+          ${platformInstallPhase}
+        '';
 
-        }
-      );
+      });# notepad-mono
+
     in rec {
+
       defaultPackage = notepad-mono;
 
       defaultApp = flake-utils.lib.mkApp {
@@ -172,5 +181,7 @@
       };
 
     }
-  );
+
+  );# outputs
+
 }
---
+++ nix/linuxPipeline.nix
@@ -0,0 +1,81 @@
+{ nixpkgs, system }:
+
+let
+
+    pkgs = import nixpkgs {
+        inherit system;
+    };
+
+    buildInputs = [
+        pkgs.flutter
+
+        pkgs.patchelf
+
+        pkgs.pango
+        pkgs.cairo
+        pkgs.glib
+        pkgs.libepoxy
+        pkgs.fontconfig
+        pkgs.gdk-pixbuf
+        pkgs.harfbuzz
+        pkgs.xorg.libX11
+        pkgs.libdeflate
+    ];
+
+    buildPhase = ''
+        cd app
+        flutter pub get
+        flutter create .
+        
+        flutter build linux --release
+    '';
+
+    installPhase = ''# $PWD starts from app directory
+        mkdir -p $out/linux
+
+        # Replace broken shared library paths with safe packages from nix store
+        for so in $PWD/build/linux/x64/release/bundle/lib/*.so; do
+          echo "Required libraries for $(basename "$so")" >> $out/linux/readelf.txt
+          
+          if readelf -d "$so" | grep -q RUNPATH; then
+            readelf -d "$so" | grep RUNPATH | tr ":" "\n" | tr "[" "\n" | tr "]" "\n" >> $out/linux/readelf.txt
+
+            patchelf --set-rpath ${pkgs.pango}/lib $so
+            patchelf --add-rpath ${pkgs.cairo}/lib $so
+            patchelf --add-rpath ${pkgs.glib}/lib $so
+            patchelf --add-rpath ${pkgs.libepoxy}/lib $so
+            patchelf --add-rpath ${pkgs.fontconfig.lib}/lib $so
+            patchelf --add-rpath ${pkgs.gdk-pixbuf}/lib $so
+            patchelf --add-rpath ${pkgs.harfbuzz}/lib $so
+            patchelf --add-rpath ${pkgs.xorg.libX11}/lib $so
+            patchelf --add-rpath ${pkgs.libdeflate}/lib $so
+            
+          else
+              echo "(no RUNPATH)" >> $out/linux/readelf.txt
+              echo " " >> $out/linux/readelf.txt
+          fi
+                        
+        done
+
+        # Copy bundle folder to output
+        cp -r $PWD/build/linux/x64/release/bundle/* $out/linux
+
+        # Patch also executable to find shared libraries
+        # readelf -d $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.cairo}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.glib}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.libepoxy}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.fontconfig.lib}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.gdk-pixbuf}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.harfbuzz}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.xorg.libX11}/lib $out/linux/notepad_mono
+        patchelf --add-rpath ${pkgs.libdeflate}/lib $out/linux/notepad_mono
+
+    '';
+
+in {
+    pkgs         = pkgs;
+    buildInputs  = buildInputs;
+    buildPhase   = buildPhase;
+    installPhase = installPhase;
+}
