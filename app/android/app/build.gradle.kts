86393cad2f711c5d0b717382fc13d9c3862701fc
MrSinho <sino3791@gmail.com>
2025-11-09 16:12:11 +0100

    Minor changes

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -30,10 +30,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "io.github.mrsinho.notepad_mono"
-        // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "io.github.mrsinho.notepad_mono"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
