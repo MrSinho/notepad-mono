10bf30e6069fdffcd1bf79c5026338d4e1e640ac
Anas Boustani <68569053+MrSinho@users.noreply.github.com>
2026-02-26 04:49:12 +0100

    Refactor to use pipeline for Notepad Mono build

--- nix/linux/default.nix
+++ nix/linux/default.nix
@@ -1,6 +1,17 @@
 { pkgs, ... }:
 
 let
-  flake = builtins.getFlake ./.;
+  pipeline = import ./pipeline.nix { inherit pkgs; };
 in
-  flake.defaultPackage
+  
+pkgs.stdenv.mkDerivation {
+
+  pname = "Notepad Mono";
+  version = "1.1.1";
+                
+  src = ./../../.;
+
+  buildInputs  = pipeline.buildInputs;
+  buildPhase   = pipeline.buildPhase;
+  installPhase = pipeline.installPhase;
+}
